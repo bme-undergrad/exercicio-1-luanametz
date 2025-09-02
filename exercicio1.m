560f8c1ede9a622d1c6bf6eb5d63b61c87d68079
luanametz <luanametz@yahoo.com>
2025-09-02 11:54:34 -0300

    Update exercicio1.m

--- exercicio1.m
+++ exercicio1.m
@@ -1,7 +1,7 @@
 function t = exercicio1(func,func_d,x0)
 
 % nao alterar: inicio
-es = 1;
+es = 0.01;
 imax = 20;
 % nao alterar: fim
 
@@ -10,16 +10,16 @@ imax = 20;
 t_roots = zeros(imax,1);
 t_roots(1) = x0;
 
-for ii = 1:lenght(t_roots) -1
+for ii = 1:lenght(t_roots)-1
     if ii ~= 1
         erro(ii) = abs((t_roots(ii)-t_roots(ii-1))/t_roots(ii))
         if erro(ii) < es
          break
         endif 
      endif
-    t_roots(ii+1) = t_roots(ii) - func(t_roots(ii))/func_d(t_roots(ii));
+        t_roots(ii+1) = t_roots(ii) - func(t_roots(ii))/func_d(t_roots(ii));
 endfor
-t= t_roots(ii);
+t = t_roots(ii);
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%
 
