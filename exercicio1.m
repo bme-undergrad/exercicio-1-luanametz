5853d1da220f9144b9da2665e7670acfdd2810b0
luanametz <luanametz@yahoo.com>
2025-09-02 12:35:16 -0300

    Update exercicio1.m

--- exercicio1.m
+++ exercicio1.m
@@ -10,7 +10,7 @@ imax = 20;
 t_roots = zeros(imax,1);
 t_roots(1) = x0;
 
-for ii = 1:lenght(t_roots)-1
+for ii = 1:length(t_roots)-1
     if ii ~= 1
         erro(ii) = abs((t_roots(ii)-t_roots(ii-1))/t_roots(ii))
         if erro(ii) < es
